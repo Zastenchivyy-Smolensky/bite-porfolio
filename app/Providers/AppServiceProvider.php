ae30a15671d15db8c2d34a417539f69ff838f564
Zastenchivyy-Smolensky <s20c2007@bene.fit.ac.jp>
2022-04-16 17:06:35 +0900

    update

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -23,6 +23,9 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        Paginator::useBootstrap(); 
+        Schema::defaultStringLength(191);
+        if(\App::enviroment("production")){
+            \URL::forceScheme("https");
+        }
     }
 }
