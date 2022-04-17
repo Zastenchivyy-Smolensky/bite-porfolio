5c18bf75a8e7f97f74e3e725d384935b607e2372
Zastenchivyy-Smolensky <s20c2007@bene.fit.ac.jp>
2022-04-18 08:58:22 +0900

    update

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -33,6 +33,7 @@
                 </button></p></a>
                 @endauth
 
+                
                 <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
                     <span class="navbar-toggler-icon"></span>
                 </button>
